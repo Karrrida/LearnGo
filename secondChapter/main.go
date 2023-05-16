4d543497603f72aeff70ebe8ad3d4256965d2429
Karrrida <trixterkorvo@gmail.com>
2023-05-16 17:47:57 +0300

    struct

--- secondChapter/main.go
+++ secondChapter/main.go
@@ -1,29 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type SuperBike struct {
+	On          bool
+	Ammo, Power int
+}
+
+func (superBike *SuperBike) Shoot() bool {
+	if superBike.On && superBike.Ammo > 0 {
+		superBike.Ammo--
+		return true
+	}
+	return false
+}
+
+func (superBike *SuperBike) RideBike() bool {
+	if superBike.On && superBike.Power > 0 {
+		superBike.Power--
+		return true
+	}
+	return false
+}
 
 func main() {
-	var a, b int = sumInt(1, 2, 3, 4)
 
-	fmt.Printf("length is %d, and sum is %d", a, b)
+	newOne := SuperBike{true, 1, 1}
+	testStruct := &newOne
+	fmt.Println(testStruct)
+	testStruct.On, testStruct.Ammo, testStruct.Power = true, 10, 10
+
+	testStruct.Shoot()
+	testStruct.RideBike()
+	fmt.Println(testStruct)
 }
 
-func sumInt(a ...int) (lenInt, sum int) {
+//func sumInt(a ...int) (lenInt, sum int) {
+//
+//	for idx := range a {
+//		lenInt++
+//		sum += a[idx]
+//	}
+//
+//	return
+//}
 
-	for idx := range a {
-		lenInt++
-		sum += a[idx]
-	}
-
-	return
-}
-
-func vote(x int, y int, z int) int {
-	var result int = 0
-
-	if x+y+z > 1 {
-		result = 1
-	}
-
-	return result
-}
+//func vote(x int, y int, z int) int {
+//	var result int = 0
+//
+//	if x+y+z > 1 {
+//		result = 1
+//	}
+//
+//	return result
+//}
