48191613f94e3770b390cb775a2e8fcb7d521eba
Karrrida <trixterkorvo@gmail.com>
2023-05-17 19:18:40 +0300

    strings

--- secondChapter/main.go
+++ secondChapter/main.go
@@ -1,40 +1,70 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type SuperBike struct {
-	On          bool
-	Ammo, Power int
-}
-
-func (superBike *SuperBike) Shoot() bool {
-	if superBike.On && superBike.Ammo > 0 {
-		superBike.Ammo--
-		return true
-	}
-	return false
-}
-
-func (superBike *SuperBike) RideBike() bool {
-	if superBike.On && superBike.Power > 0 {
-		superBike.Power--
-		return true
-	}
-	return false
-}
+//type SuperBike struct {
+//	On          bool
+//	Ammo, Power int
+//}
+//
+//func (s *SuperBike) Shoot() bool {
+//	if s.On && s.Ammo > 0 {
+//		s.Ammo--
+//		return true
+//	}
+//	return false
+//}
+//
+//func (s *SuperBike) RideBike() bool {
+//	if s.On && s.Power > 0 {
+//		s.Power--
+//		return true
+//	}
+//	return false
+//}
 
 func main() {
+	//newOne := SuperBike{true, 1, 1}
+	//testStruct := &newOne
+	//fmt.Println(testStruct)
+	//testStruct.On, testStruct.Ammo, testStruct.Power = true, 10, 10
+	//testStruct.Shoot()
+	//testStruct.RideBike()
+	//fmt.Println(testStruct)
+	//inp, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	//isLegalString := "Wrong"
+	//sr := []rune(inp)
+	//
+	//if unicode.IsUpper(sr[0]) && string(sr[len(sr)-3]) == "." {
+	//	isLegalString = "Right"
+	//}
+	//fmt.Println(isLegalString)
 
-	newOne := SuperBike{true, 1, 1}
-	testStruct := &newOne
-	fmt.Println(testStruct)
-	testStruct.On, testStruct.Ammo, testStruct.Power = true, 10, 10
+	//var inpStr string
+	//
+	//fmt.Scan(&inpStr)
+	//
+	//sr2 := []rune(inpStr)
+	//
+	//if string(sr2[0]) == string(sr2[len(sr2)-1]) {
+	//	fmt.Println("Палиндром")
+	//} else {
+	//	fmt.Println("Нет")
+	//}
 
-	testStruct.Shoot()
-	testStruct.RideBike()
-	fmt.Println(testStruct)
+	var str string
+
+	fmt.Scan(&str)
+	//var resStr string
+	rs := []rune(str)
+	var resRs []rune
+	for idx, val := range rs {
+		if idx%2 != 0 {
+			resRs = append(resRs, val)
+		}
+	}
+	fmt.Print(string(resRs))
+
 }
 
 //func sumInt(a ...int) (lenInt, sum int) {
